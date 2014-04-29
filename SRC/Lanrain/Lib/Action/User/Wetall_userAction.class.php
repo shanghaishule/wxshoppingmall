ee52d5804a62fb17252ba508a78e7b43739aaf5b
polo2013 <paul.zhou@163.com>
2014-04-29 15:55:06 +0800

    会员管理

---
+++ SRC/Lanrain/Lib/Action/User/Wetall_userAction.class.php
@@ -0,0 +1,80 @@
+<?php
+class Wetall_userAction extends UserAction{
+	public function _initialize() {
+		parent::_initialize();
+		$this->_mod = D('user');
+	}
+	
+	
+	public function index(){
+		$tokenTall = $this->getTokenTall();
+		$map = array();
+		$map['role'] = array('eq','0');
+		$mod = $this->_mod;
+		!empty($mod) && $this->_list($mod, $map);
+		$this->display();
+	}
+
+	public function edit() {
+		$id = $this->_get('id');
+		$tokenTall = $this->getTokenTall();
+		
+		//提交，有id则为编辑，无id则为新增
+		if (IS_POST) {
+			//dump($_POST);exit;
+			 
+			//获取数据
+			if (false === $data = $this->_mod->create()) {
+				$this->error($this->_mod->getError());
+			}
+			$data['tokenTall'] = $tokenTall;
+			
+			if ($_POST['id'] != "") {
+				//编辑
+				$result = $this->_mod->save($data);
+			} else {
+				//新增
+				$result = $this->_mod->add($data);
+			}
+
+			if ($result !== false) {
+				$this->success('成功！', U('Wetall_user/index'));
+			} else {
+				$this->error('失败！');
+			}
+			
+		} 
+		//非提交，有id为编辑展示，无id为新增展示
+		else {
+			if ($id) {
+				$myaction = "编辑";
+				$info = $this->_mod->where(array('id'=>$id))->find();
+				$this->assign('info',$info);
+				
+			}else{
+				$myaction = "新增";
+			}
+						
+			$this->assign('myaction',$myaction);
+			$this->display();
+		}
+	}
+	
+	public function del()
+	{
+	
+		$ids = $this->_get('id');
+		if ($ids) {
+			
+			if (false !== $this->_mod->delete($ids)) {
+				$this->success('删除成功！');
+			} else {
+				$this->error('删除失败！');
+			}
+		} else {
+			$this->error('参数错误！');
+		}
+	}
+
+}
+?>
