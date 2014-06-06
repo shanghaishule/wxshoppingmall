d8352c6a1a787795e8ed6f1b1f8c6da203b8c6f2
ranyigit <ranyi_num@163.com>
2014-06-06 15:55:44 +0800

    test

---
+++ SRC/weTall/static/js/jQuery-shopping.js
@@ -0,0 +1,54 @@
+
+;(function($){
+	$.extend($.fn,{
+		shoping:function(options){
+			var self=this,
+				$shop=$('#n_3'),
+				//$title=$('.J-shoping-title'),
+				//$body=$('.J-shoping-body'),
+				//$num=$('.J-shoping-num'),
+				$close=$('.J-shoping-close');
+			var S={
+				init:function(){
+					$(self).data('click',true).live('click',this.addShoping);
+					$(document).bind('click',S.slideBoxMini);
+					$body.bind('click',this.clickOnBody);
+				},
+				clickOnBody:function(e){
+					if(!$(e.target).hasClass('J-shoping-close')){
+						e.stopPropagation(); //阻止冒泡	
+					};
+				},
+				addShoping:function(e){
+					e.stopPropagation();
+					var $target=$(e.target),
+						id=$target.attr('id'),
+						dis=$target.data('click'),
+					    x = $target.offset().left + 30,
+						y = $target.offset().top + 10,
+						X = $shop.offset().left+$shop.width()/2-$target.width()/2+10,
+						Y = $shop.offset().top;
+					if(dis){
+						if ($('#floatOrder').length <= 0) {
+							$('body').append('<div id="floatOrder"><img src="images/pro1.jpg" width="50" height="50" /></div');
+						};
+						var $obj=$('#floatOrder');
+						if(!$obj.is(':animated')){
+							$obj.css({'left': x,'top': y}).animate({'left': X,'top': Y-80},500,function() {
+								$obj.stop(false, false).animate({'top': Y-20,'opacity':0},500,function(){
+									$obj.fadeOut(300,function(){
+										$obj.remove();	
+										//	num=Number($num.text());
+										//$num.text(num+1);
+									});
+								});
+							});	
+						};
+					};
+				},
+			};
+			S.init(); 
+		}
+	});	
+})(jQuery);
+
