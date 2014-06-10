351a6f1c1079ecfb2e8f74b14f5d15434e1799a3
ranyigit <ranyi_num@163.com>
2014-06-10 11:30:48 +0800

    test

--- SRC/weTall/app/Lib/Action/home/bookAction.class.php
+++ SRC/weTall/app/Lib/Action/home/bookAction.class.php
@@ -214,9 +214,9 @@ class bookAction extends frontendAction {
     	$where['status']=array('eq',1);
     	$where['tokenTall']=array('eq',$tokenTall);
     	$items = M('item')->field('id,title,img,price')->order('ordid asc,id desc')->where($where)->limit($this->row*($this->page-1),$this->row)->select();
-    	foreach($items as $item){
+    	foreach($items as $item => $value){
     		$lv=M('item_like')->where("item_id='".$item['id']."'")->count();
-    	    $items[]['lv']=$lv;
+    	    $items[$item]['lv']=$lv;
     	}
     	echo json_encode($items);
     	 
